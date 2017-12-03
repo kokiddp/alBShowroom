71c4c93491ee9f112253b3b7563b12a25465f328
Gabriele Coquillard <gabriele.digival@gmail.com>
2017-12-03 16:53:56 +0100

    boilerplate

---
+++ includes/class-alb-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/kokiddp/
+ * @since      1.0.0
+ *
+ * @package    Alb
+ * @subpackage Alb/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Alb
+ * @subpackage Alb/includes
+ * @author     Gabriele Coquillard <gabriele.coquillard@gmail.com>
+ */
+class Alb_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ includes/class-alb-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://github.com/kokiddp/
+ * @since      1.0.0
+ *
+ * @package    Alb
+ * @subpackage Alb/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Alb
+ * @subpackage Alb/includes
+ * @author     Gabriele Coquillard <gabriele.coquillard@gmail.com>
+ */
+class Alb_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'alb',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
