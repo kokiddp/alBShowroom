1a58984b0a5d330ca25ea46008fc0539b3f0477b
Gabriele Coquillard <gabriele.digival@gmail.com>
2017-12-16 19:00:01 +0100

    fix per compatibilità

--- public/js/alb-public.js
+++ public/js/alb-public.js
@@ -31,26 +31,31 @@
 
 
 	$(document).ready(function() {
-		var supportedFlag = $.keyframe.isSupported();
 
-		var scrolling_p = $(".marquee > p");
+		if (window.location.href.indexOf("taplist") > -1) {
 
-		$.each(scrolling_p, function(index, value) {
+	       var supportedFlag = $.keyframe.isSupported();
 
-			if ($(this).outerWidth(true) > $(this).parent().outerWidth(true)) {
+			var scrolling_p = $(".marquee > p");
 
-				var start = 100 / $(this).outerWidth(true) * $(this).parent().outerWidth(true);
-				var speed = $(this).outerWidth(true) / $(this).parent().outerWidth(true) * 7;
-				$.keyframe.define([{
-				    name: 'albmarquee-' + index,
-				    '0%': {'transform': 'translate3d(' + start + '%, 0, 0)'},
-				    '100%': {'transform': 'translate3d(-100%, 0, 0)'}
-				}]);
-				$(this).css('text-align', 'center').css('animation', 'albmarquee-' + index + ' ' + speed + 's linear infinite');
-				$(this).parent().css('overflow', 'hidden');
-			}
+			$.each(scrolling_p, function(index, value) {
 
-		});
+				if ($(this).outerWidth(true) > $(this).parent().outerWidth(true)) {
+
+					var start = 100 / $(this).outerWidth(true) * $(this).parent().outerWidth(true);
+					var speed = $(this).outerWidth(true) / $(this).parent().outerWidth(true) * 7;
+					$.keyframe.define([{
+					    name: 'albmarquee-' + index,
+					    '0%': {'transform': 'translate3d(' + start + '%, 0, 0)'},
+					    '100%': {'transform': 'translate3d(-100%, 0, 0)'}
+					}]);
+					$(this).css('text-align', 'center').css('animation', 'albmarquee-' + index + ' ' + speed + 's linear infinite');
+					$(this).parent().css('overflow', 'hidden');
+				}
+
+			});
+	    }
+		
 	});
 
 })( jQuery );
--- public/partials/alb-public-taplist-display.php
+++ public/partials/alb-public-taplist-display.php
@@ -36,7 +36,7 @@ $url = str_replace( 'partials/', '', plugin_dir_url( __FILE__ ) );
   	src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
   	integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
   	crossorigin="anonymous"></script>
-  <script src="<?= $url . 'js/jquery.keyframes.js' ?>"></script>
+  <script src="<?= $url . 'js/jquery-keyframes.js' ?>"></script>
   <script src="<?= $url . 'js/alb-public.js' ?>"></script>
 </head>
 <body>
